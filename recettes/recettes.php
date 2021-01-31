7ed824d1ae03db24857517331ea9a11555a846bf
QuentinB1530 <77584944+QuentinB1530@users.noreply.github.com>
2021-01-31 22:39:36 +0100

    Add files via upload

---
+++ recettes/recettes.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+
+
+
+	<head>
+
+		<meta charset ="UTF-8"/>
+
+		<link rel="stylesheet" type=text/css href="css/css.css"/>
+
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+
+		<title>Les Recettes du Chef</title>
+
+	</head>
+
+
+
+	<body>
+
+
+
+		<header>
+			<?php include("includes_recettes/header.php"); ?>
+		</header>
+
+		
+
+		<h3>Choisissez quel type de recette vous avez envie de cuisiner aujourd'hui !</h3>
+
+		<h4>Attention ce site est encore en travaux !</h4>
+
+		
+
+		<footer>
+			<?php include("includes_recettes/footer.php"); ?>
+		</footer>
+
+
+
+	</body>
+
+
+
+</html>
